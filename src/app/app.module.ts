cbde697f0896fceecb6a56bd779b7adf7f350086
MinhKhoi <mkhoi1776@gmail.com>
2023-06-11 16:02:09 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
